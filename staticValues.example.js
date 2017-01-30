51b3ccaf7dd5e085d1250cecb9c38405eafcfc24
kalebhermes <hermes.kaleb@gmail.com>
2017-01-30 16:08:19 -0500

    Added staticValues.js

---
+++ staticValues.example.js
@@ -0,0 +1,125 @@
+module.exports = {
+
+    "botToken" : '',
+    
+    "symbols" : {
+        'Success': '<:success:273460303755214848>',
+        'Advantage': '<:advantage:273460264039612416>',
+        'Failure': '<:failure:273460289326940171>',
+        'Threat': '<:threat:273460313071026186>',
+        'Triumph': '<:triumph:273460319366545409>',
+        'Despair': '<:despair:273460282309738496>',
+        'Black': '<:darkside:273460274055348225>',
+        'White': '<:lightside:273460296981413888>',
+        'Blank': '',
+    },
+
+    "diceArray" : {
+      'g': {
+          'sides':8,
+          'emoji':'<:abilitybg:273460177762648074>',
+          'faces':{
+            1: 'Blank',
+            2: 'Success',
+            3: 'Success',
+            4: 'Success Success', 
+            5: 'Advantage',
+            6: 'Advantage',
+            7: 'Advantage Success',
+            8: 'Advantage Advantage'
+            }
+        },
+      'y': {
+          'sides':12,
+          'emoji':'<:proficiencybg:273460214743695360>',
+          'faces':{
+            1: 'Blank',
+            2: 'Success',
+            3: 'Success Success',
+            4: 'Success Success',
+            5: 'Advantage',
+            6: 'Advantage Success',
+            7: 'Advantage Success',
+            8: 'Advantage Success',
+            9: 'Advantage Advantage',
+            10: 'Advantage Advantage',
+            11: 'Advantage Advantage',
+            12: 'Triumph'
+            }
+        },
+      'b': {
+          'sides':6,
+          'emoji':'<:boostbg:273460186272759808>',
+          'faces':{
+            1: 'Blank',
+            2: 'Blank',
+            3: 'Success',
+            4: 'Advantage Success',
+            5: 'Advantage',
+            6: 'Advantage Advantage'
+            }
+        },
+      'p': {
+          'sides':8,
+          'emoji':'<:difficultybg:273460199862435841>',
+          'faces':{
+            1: 'Blank',
+            2: 'Failure',
+            3: 'Failure Failure',
+            4: 'Threat', 
+            5: 'Threat',
+            6: 'Threat',
+            7: 'Threat Threat',
+            8: 'Threat Failure'
+            }
+        },
+      'r': {
+          'sides':12,
+          'emoji':'<:challengebg:273460192811810817>',
+          'faces':{
+            1: 'Blank',
+            2: 'Failure',
+            3: 'Failure',
+            4: 'Failure Failure',
+            5: 'Failure Failure',
+            6: 'Threat',
+            7: 'Threat',
+            8: 'Threat Failure',
+            9: 'Threat Failure',
+            10: 'Threat Threat',
+            11: 'Threat Threat',
+            12: 'Despair'
+            }
+        },
+      'k': {
+          'sides':6,
+          'emoji':'<:setbackbg:273460221458907136>',
+          'faces':{
+            1: 'Blank',
+            2: 'Blank',
+            3: 'Failure',
+            4: 'Failure',
+            5: 'Threat',
+            6: 'Threat'
+            }
+        },
+      'f': {
+          'sides':12,
+          'emoji':'<:forcebg:273460207546400769>',
+          'faces':{
+            1: 'Black',
+            2: 'Black',
+            3: 'Black',
+            4: 'Black',
+            5: 'Black',
+            6: 'Black',
+            7: 'Black Black',
+            8: 'White',
+            9: 'White',
+            10: 'White White',
+            11: 'White White',
+            12: 'White White'
+            }
+        }
+    }
+}
